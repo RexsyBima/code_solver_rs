0e005c094426776140207effa954aae2775971f0
Rexsxy Bima <Rexsy.Bimq12@gmail.com>
2025-07-21 11:07:23 +0700

    added function to check if we are connected to internet

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,25 @@ use deepseek_rs::{
 use image::{DynamicImage, RgbaImage};
 use std::env;
 use std::error::Error;
+use std::net::TcpStream;
 use std::os;
 use win_screenshot::prelude::*;
+
+fn check_internet() {
+    let urls = vec![("8.8.8.8", 53), ("google.com", 80)];
+    let mut connected = false;
+    for (host, port) in urls {
+        let stream = TcpStream::connect((host, port));
+        if stream.is_ok() {
+            connected = true;
+            break;
+        }
+    }
+    if !connected {
+        println!("No internet connection");
+        std::process::exit(1);
+    }
+}
 
 fn takescreenshot() {
     let buf = capture_display().unwrap();
@@ -17,13 +34,16 @@ fn takescreenshot() {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    check_internet();
     dotenvy::dotenv()?;
+    // https://docs.rs/openai-api-rs/6.0.7/
+    //https://crates.io/crates/ocrs
     let client = DeepSeekClient::default()?;
     takescreenshot();
     let question = String::from("Description:
 
-Given an array of integers nums and an integer k, return the length of the longest contiguous subarray whose sum equals k.
-If no such subarray exists, return 0. please answer it in python");
+    Given an array of integers nums and an integer k, return the length of the longest contiguous subarray whose sum equals k.
+    If no such subarray exists, return 0. please answer it in python");
     let request = RequestBody::new_messages(vec![Message::new_user_message(question)])
         .with_model(Model::DeepseekChat);
     let response = client.chat_completions(request).await?;
